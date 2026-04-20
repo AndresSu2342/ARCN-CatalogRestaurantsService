6357fff46c5e641b6e5c4ee4fd4c6616eea29f9f
Andres Suarez <andresborray123@gmail.com>
2026-04-20 16:25:58 -0500

    First commit

---
+++ src/main/java/arcn/CatalogRestaurantsService/CatalogRestaurantsServiceApplication.java
@@ -0,0 +1,13 @@
+package arcn.CatalogRestaurantsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogRestaurantsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogRestaurantsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arcn/CatalogRestaurantsService/CatalogRestaurantsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package arcn.CatalogRestaurantsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogRestaurantsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
